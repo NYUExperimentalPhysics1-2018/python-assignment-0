4ae26531c4f04c88059056e6cee2f53b517dbf7b
Marc Gershow <gershow@gmail.com>
2018-08-31 15:47:05 -0400

    added style guidelines and ps0 starter

---
+++ ps0.py
@@ -0,0 +1,21 @@
+"""
+ps0.py
+    starter template for Python Problem Set 0
+    modify with your own work and submit via GitHub
+
+
+    @author: Your Name
+    TA: Your TAs Name
+    Assignment: PS0
+"""
+
+# replace this commented section with your source code
+# when you run the code, it should ask for 2 numbers, x&y
+# then it should print out x^y and log_2(x)
+# see handout: MIT6_0001F16_ProblemSet0.pdf in this directory for instructions
+#
+
+#optional extensions: 
+#   check to make sure x > 0 and re-prompt user if a negative value is entered
+#   ask the user if (s)he wants to run the program again and loop until the answer is 'n'
+#   ask the user for a name and call the user by name when asking for input
